62dbda798110ba0ffb774d07bc8963e195510741
Rachel Kim <kimrache@umich.edu>
2016-11-10 23:01:18 -0500

    twitterhw3a.py and corgi picture

--- HW3-StudentCopy/twitterhw3a.py
+++ HW3-StudentCopy/twitterhw3a.py
@@ -6,3 +6,23 @@
 
 print("""No output necessary although you 
 	can print out a success/failure message if you want to.""")
+
+import tweepy
+import nltk
+
+# Unique code from Twitter
+access_token = "796822723083325440-CObpW1SgnHGahZofbWPehr477q4zLnD"
+access_token_secret = "EQuKPjhoV8pf79n8sWEJ1QSkFouQWQBkt9vfcbwIkzZfn"
+consumer_key = "ghPC3Y6S9EcfTcPLKy9l9fptD"
+consumer_secret = "3bMOGDoQDrqDHzYW0ycO3O7YypOFIkmCkpHIVreqJCtPpaXZtl"
+
+# Boilerplate code here
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+#Now we can Create Tweets, Delete Tweets, and Find Twitter Users
+
+filename = "corgi.jpg"
+
+api.update_with_media(filename, status="#UMSI-206 #Proj3")
