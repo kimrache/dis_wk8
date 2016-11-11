9c073bcb8d413876d409e8c3659d022329c22f28
Rachel Kim <kimrache@umich.edu>
2016-11-10 23:17:20 -0500

    twitterhw3b.py final

--- HW3-StudentCopy/twitterhw3b.py
+++ HW3-StudentCopy/twitterhw3b.py
@@ -7,6 +7,36 @@
 
 # Be prepared to change the search term during demo.
 
+import tweepy
+from textblob import TextBlob
 
-print("Average subjectivity is")
-print("Average polarity is")
+# Unique code from Twitter
+access_token = "796822723083325440-CObpW1SgnHGahZofbWPehr477q4zLnD"
+access_token_secret = "EQuKPjhoV8pf79n8sWEJ1QSkFouQWQBkt9vfcbwIkzZfn"
+consumer_key = "ghPC3Y6S9EcfTcPLKy9l9fptD"
+consumer_secret = "3bMOGDoQDrqDHzYW0ycO3O7YypOFIkmCkpHIVreqJCtPpaXZtl"
+
+# Boilerplate code here
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+#Now we can Create Tweets, Delete Tweets, and Find Twitter Users
+
+search = input("Enter a term to search for: ")
+public_tweets = api.search(search)
+
+
+total_subjectivity = 0;
+total_polarity = 0;
+total_ct = 0;
+
+for tweet in public_tweets:
+	print(tweet.text)
+	analysis = TextBlob(tweet.text)
+	total_polarity += analysis.sentiment.polarity
+	total_subjectivity += analysis.sentiment.subjectivity
+	total_ct += 1;
+
+print("Average subjectivity is", total_subjectivity / total_ct)
+print("Average polarity is", total_polarity / total_ct)
